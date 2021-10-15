41822ae9f1906a8185409c81937f06dffbbbdba2
cahidenes <cahideneskeles54@gmail.com>
2021-10-15 17:21:17 +0300

    Add boolean, dropdown and boolean widgets

--- dynamic_variables.py
+++ dynamic_variables.py
@@ -8,6 +8,19 @@ def get_setter(obj, name):
     return setter
 
 
+def get_text_callback(obj, name, entry):
+    def callback():
+        obj.window.after(10, lambda: setattr(obj, name, entry.get()))
+        return True
+    return callback
+
+
+def get_boolean_callback(obj, name, var):
+    def callback():
+        setattr(obj, name, var.get())
+    return callback
+
+
 class VariableTweaker:
     def __init__(self):
         self.create_requests = []
@@ -15,6 +28,15 @@ class VariableTweaker:
 
     def add_slider(self, name, value, min_value, max_value, step):
         self.create_requests.append(('slider', (name, value, min_value, max_value, step)))
+
+    def add_text(self, name, value):
+        self.create_requests.append(('text', (name, value)))
+
+    def add_dropdown(self, name, value, options):
+        self.create_requests.append(('dropdown', (name, value, options)))
+
+    def add_boolean(self, name, value):
+        self.create_requests.append(('boolean', (name, value)))
 
     def init_gui_thread(self):
         for request_name, parameters in self.create_requests:
@@ -29,14 +51,45 @@ class VariableTweaker:
                                orient=tk.HORIZONTAL, command=get_setter(self, name))
                 scl.set(value)
                 scl.pack()
-                variables.append((name, scl))
+                variables.append((request_name, name, scl))
+            elif request_name == 'text':
+                name, value = parameters
+                entry = tk.Entry(self.window)
+                entry.configure(validate='key', validatecommand=get_text_callback(self, name, entry))
+                entry.insert(0, value)
+                entry.pack()
+                variables.append((request_name, name, entry))
+            elif request_name == 'dropdown':
+                name, value, options = parameters
+                variable = tk.Variable(value=value, name=name)
+                optionmenu = tk.OptionMenu(self.window, variable, *options, command=get_setter(self, name))
+                optionmenu.pack()
+                variables.append((request_name, name, optionmenu))
+            elif request_name == 'boolean':
+                name, value = parameters
+                var = tk.BooleanVar(value=value)
+                checkbutton = tk.Checkbutton(self.window, text=name, variable=var)
+                checkbutton.configure(command=get_boolean_callback(self, name, var))
+                checkbutton.pack()
+                variables.append((request_name, name, checkbutton))
 
         def constant_checker():
-            for i, (variable_name, scale) in enumerate(variables):
+            for request_name, variable_name, widget in variables:
                 new_value = getattr(self, variable_name)
-                if new_value != scale.get():
-                    variables[i] = (variable_name, scale)
-                    scale.set(new_value)
+                if request_name == 'slider':
+                    if new_value != widget.get():
+                        widget.set(new_value)
+                # elif request_name == 'text':
+                #     if new_value != widget.get():
+                #         widget.delete(0, len(widget.get()))
+                #         widget.insert(0, new_value)
+                # elif request_name == 'dropdown':
+                #     if new_value != widget.get():
+                #         widget.set(new_value)
+                # elif request_name == 'boolean':
+                #     if new_value != widget.get():
+                #         widget.set(new_value)
+
             self.window.after(100, constant_checker)
 
         self.window.after(100, constant_checker)
--- test.py
+++ test.py
@@ -2,8 +2,10 @@ import dynamic_variables
 import time
 
 gui1 = dynamic_variables.VariableTweaker()
-gui1.add_slider('var1', 5, 0, 10, 0.1)
-gui1.add_slider('var2', 1, 0, 10, 0.1)
+gui1.add_slider('slidervar', 5, 0, 10, 0.1)
+gui1.add_text('textvar', 'asdf')
+gui1.add_dropdown('dropdownvar', 1, [1, 'abc', True, False])
+gui1.add_boolean('booleanvar', True)
 
 gui1.init_gui()
 
@@ -12,5 +14,5 @@ gui1.init_gui()
 # gui2.init_gui()
 
 while True:
-    print(gui1.var1, gui1.var2)
+    print(gui1.slidervar, gui1.textvar, gui1.booleanvar, gui1.dropdownvar)
     time.sleep(1)
